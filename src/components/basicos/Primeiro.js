32bbe7264b53873089dd9054983942d45730b400
Thomas Silva <thomas@allto.digital>
2023-11-08 15:43:13 -0300

    6- Primeiro componente #2

--- src/components/basicos/Primeiro.js
+++ src/components/basicos/Primeiro.js
@@ -1,3 +1,12 @@
+import React from "react";
+
 export default function Primeiro() {
-  return "Primeiro Componente";
+  const msg = "Seja bem vindo(a)!";
+
+  return (
+    <div>
+      <h2>Primeiro Componente</h2>
+      <p>{msg}</p>
+    </div>
+  );
 }

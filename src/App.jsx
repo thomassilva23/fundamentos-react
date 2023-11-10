56fd163b93b45bd517d68190d9b8e63377f0c7f1
Thomas Silva <thomas@allto.digital>
2023-11-10 13:46:03 -0300

    12- Desafio Número Aleatório

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 
-import Primeiro from "./components/basicos/Primeiro";
-import ComParametro from "./components/basicos/ComParametro";
+import Aleatorio from "./components/basicos/Aleatorio";
 import Fragmento from "./components/basicos/Fragmento";
+import ComParametro from "./components/basicos/ComParametro";
+import Primeiro from "./components/basicos/Primeiro";
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default () => (
   <div id="app">
-    <h1>Fundamentos React 2 (Arrow _)</h1>
+    <h1>Fundamentos React</h1>
+    <Aleatorio min={10} max={60} />
     <Fragmento />
     <ComParametro titulo="Segundo Componente" aluno="Pedro Silva" nota={9.3} />
     <Primeiro />
---
+++ src/components/basicos/Aleatorio.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (props) => {
+  const { min, max } = props;
+
+  const aleatorio = parseInt(Math.random() * (max - min)) + min;
+  return (
+    <div>
+      <h2>Valor Aleatório</h2>
+      <p>
+        <strong>Valor Mínimo: </strong> {min}
+      </p>
+      <p>
+        <strong>Valor Máximo: </strong> {max}
+      </p>
+      <p>
+        <strong>Valor Escolhido: </strong> {aleatorio}
+      </p>
+    </div>
+  );
+};

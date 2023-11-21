a9d00b24e11fd6ff54acf218b666b8a027c827ef
Thomas Silva <thomas@allto.digital>
2023-11-21 16:48:37 -0300

    17- Componente com Filho #02

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import React from "react";
 import Card from "./components/layout/Card";
 
 import Familia from "./components/basicos/Familia";
-
+import FamiliaMembro from "./components/basicos/FamiliaMembro";
 import Aleatorio from "./components/basicos/Aleatorio";
 import Fragmento from "./components/basicos/Fragmento";
 import ComParametro from "./components/basicos/ComParametro";
@@ -17,7 +17,11 @@ export default () => (
 
     <div className="Cards">
       <Card titulo="#05 - Componente com Filhos" color="#00C8F8">
-        <Familia sobrenome="Ferreira"></Familia>
+        <Familia sobrenome="Silva">
+          <FamiliaMembro nome="Pedro" />
+          <FamiliaMembro nome="Ana" />
+          <FamiliaMembro nome="Gustavo" />
+        </Familia>
       </Card>
 
       <Card titulo="#04 - Desafio Aleatório" color="#FA6900">

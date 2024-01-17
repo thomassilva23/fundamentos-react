08e42f5475879f792ef34cd7cf5537c001da388a
Thomas Silva <thomas@allto.digital>
2024-01-17 15:34:57 -0300

    18 - Repetição

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from "react";
 
 import Card from "./components/layout/Card";
 
+import ListaAlunos from "./components/repeticao/ListaAlunos";
 import Familia from "./components/basicos/Familia";
 import FamiliaMembro from "./components/basicos/FamiliaMembro";
 import Aleatorio from "./components/basicos/Aleatorio";
@@ -16,6 +17,10 @@ export default () => (
     <h1>Fundamentos React</h1>
 
     <div className="Cards">
+      <Card titulo="#06 - Repetição" color="#FF4C65">
+        <ListaAlunos></ListaAlunos>
+      </Card>
+
       <Card titulo="#05 - Componente com Filhos" color="#00C8F8">
         <Familia sobrenome="Silva">
           <FamiliaMembro nome="Pedro" />

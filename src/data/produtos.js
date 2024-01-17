987835cb7be5657a741261910b140ebc3e58f748
Thomas Silva <thomas@allto.digital>
2024-01-17 15:50:25 -0300

    20 - Desafio Repetição

---
+++ src/data/produtos.js
@@ -0,0 +1,13 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default [
+  { id: 1, nome: "Caneta", preco: 2.99 },
+  { id: 2, nome: "Lápis", preco: 1.99 },
+  { id: 3, nome: "iPad", preco: 5899.99 },
+  { id: 4, nome: "Samsung S20+", preco: 6599.99 },
+  { id: 5, nome: "Notebook", preco: 3499.99 },
+  { id: 6, nome: "Caderno", preco: 19.99 },
+  { id: 7, nome: "Borracha", preco: 4.99 },
+  { id: 8, nome: "Impressora", preco: 889.99 },
+  { id: 9, nome: "Monitor 27", preco: 799.99 },
+  { id: 10, nome: "Cadeira", preco: 1239.99 },
+];
